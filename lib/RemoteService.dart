b9ff27eb0818abaaee4dd420ec8902e02d888f65
hemant Kumar <hemantkumar692@gmail.com>
2022-09-02 18:40:00 +0530

    static part is done.

--- lib/RemoteService.dart
+++ lib/RemoteService.dart
@@ -6,39 +6,88 @@ import 'package:dio/dio.dart';
 import 'package:freelance/firstModel/firstModel.dart';
 class RemoteService {
 
- static var token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjU4M2YxZTMxLTI3ZDEtNDI0NS1iNjNiLTE3NzA4Nzc1YzgzMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkEiLCJleHAiOjE2NjIxMDE1MDMsImlzcyI6IlVzZXJDZW50ZXIiLCJhdWQiOiJhbGwifQ.QADFEE6N3tPAoSfy2Jr8Fgtj2HgzORxeHVViLyDT5wA";
   static var client = dio.Dio();
-  static Future<FirstModel?> getparent() async{
-    String theUrl = "https://testapi.cptinternational.com/api/Agents/SearchAgentTree";
-    var formData = FormData.fromMap({
-           "account": "603273",
-          "businessType": "FT_A",
-          "keyword": "603273",
-          "search": "Y"
-    });
-    try {
-      dio.Response response = await client
-          .post(theUrl,
-          options:
-          dio.Options(
-              headers: {
-                // 'Authorization': 'Bearer$token'
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': 'Bearer $token',
-              }
-              ),data: formData)
-          .timeout(const Duration(seconds: 30));
+  static var token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6Ijk1ZTMwZGJmLTdmNjAtNDgxYy04NzMwLTU0NDVlOTk4ZmIwYSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkMiLCJleHAiOjE2NjIyMDE0MjMsImlzcyI6IlVzZXJDZW50ZXIiLCJhdWQiOiJhbGwifQ.LliP1WvK9Ch-ykdDhosTBmdVnekVIfoePSnFdaioZ4A";
+ Dio? _dio;
+ RemoteService(){
+   _dio = Dio();
+ }
+ static Future <List<Data>?> getparentlist() async{
+   String theUrl = "https://testapi.cptinternational.com/api/Agents/SearchAgentTree";
+   try{
+     client.options.headers['content-Type'] = 'application/json';
+     client.options.headers['Accept'] = 'application/json';
+     client.options.headers["authorization"]='Bearer $token';
 
-      if (response.statusCode == 200) {
-        print("Success hemant");
-        //     return PhoneNo(status: data['status'],message: data['message']);
-        return FirstModel.fromJson(response.data);
-      } else {
-        return null;
+     Response response = await client.post(theUrl,data: {
+       "account": "603273",
+       "businessType": "FT_A",
+       "keyword": "603273",
+       "search": "Y"
+     });
+     if(response.statusCode==200){
+       FirstModel todayvisitModel = FirstModel.fromJson(response.data);
+       return todayvisitModel.data;
+     }
+
+   } on DioError catch(e){
+     print(e);
+   }
+ }
+  static Future <List<Data>?> getchildlist() async{
+    String theUrl = "https://testapi.cptinternational.com/api/Agents/SearchAgentTree";
+    try{
+      client.options.headers['content-Type'] = 'application/json';
+      client.options.headers['Accept'] = 'application/json';
+      client.options.headers["authorization"]='Bearer $token';
+
+      Response response = await client.post(theUrl,data: {
+        "account": "603273",
+        "businessType": "FT_A",
+        "keyword": "603273",
+        "search": "N"
+      });
+      if(response.statusCode==200){
+        FirstModel todayvisitModel = FirstModel.fromJson(response.data);
+        return todayvisitModel.data;
       }
-    } on TimeoutException catch (_) {
-      return null;
+
+    } on DioError catch(e){
+      print(e);
     }
   }
+  // static Future<FirstModel?> getparent() async{
+  //   String theUrl = "https://testapi.cptinternational.com/api/Agents/SearchAgentTree";
+  //   var formData = FormData.fromMap({
+  //          "account": "603273",
+  //         "businessType": "FT_A",
+  //         "keyword": "603273",
+  //         "search": "Y"
+  //   });
+  //   try {
+  //     dio.Response response = await client
+  //         .post(theUrl,
+  //         options:
+  //         dio.Options(
+  //             headers: {
+  //               // 'Authorization': 'Bearer$token'
+  //               'Content-Type': 'application/json',
+  //               'Accept': 'application/json',
+  //               'Authorization': 'Bearer $token',
+  //               'lang': 'en-us'
+  //             }
+  //             ),data: formData)
+  //         .timeout(const Duration(seconds: 30));
+  //
+  //     if (response.statusCode == 200) {
+  //       print("Success hemant");
+  //       //     return PhoneNo(status: data['status'],message: data['message']);
+  //       return FirstModel.fromJson(response.data);
+  //     } else {
+  //       return null;
+  //     }
+  //   } on TimeoutException catch (_) {
+  //     return null;
+  //   }
+  // }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
-  List<Data>datalist =[];
+
 
   // This widget is the root of your application.
   @override
@@ -37,11 +37,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Data>datalist =[];
+  List<Data>childlist=[];
 
   @override
   void initState() {
-    apiparent();
-   // RemoteService.getparent();
+//  apiparent();
+  RemoteService.getparentlist().then((value) {
+    setState(() {
+      datalist.addAll(value!);
+    });
+  });
+  // setState(() {
+  //
+  //   // FirstModel aa = FirstModel();
+  //   // datalist= aa.data!;
+  //  // datalist= RemoteService.getTodaylist() as List<Data>;
+  // });
+
     super.initState();
   }
 
@@ -53,37 +66,123 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
       ),
-      body: ListView(
-        children:  basicTiles.map(buildTile).toList(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: ListView.builder(
+            itemCount: datalist.length,
+            itemBuilder: (context, index) {
+              return ExpansionTile(
+                trailing: const SizedBox.shrink(),
+                leading: const Icon(Icons.add),
+                title: Text(datalist[index].agentId.toString()+"     "+datalist[index].userName.toString()+"     "+datalist[index].balance.toString()+" "
+                    "    "+datalist[index].equity.toString(),style: TextStyle(color: Colors.red,fontSize: 16),),
+                onExpansionChanged: (bool open) async {
+                  if(open){
+                    RemoteService.getchildlist().then((value) {
+                      setState(() {
+                        childlist.addAll(value!);
+                      });
+                    });
 
 
+                  }
+                },
+                children: [
+                 FutureBuilder<List<Data>?>(
+                   future: RemoteService.getchildlist(),
+                   builder: (context, snapshot) {
+
+                    if(snapshot.connectionState==ConnectionState.waiting){
+                      return Center(child:  CircularProgressIndicator(),);
+                    }
+                     else {
+                     return  ListView.builder(
+                         shrinkWrap: true,
+                         physics: NeverScrollableScrollPhysics(),
+                         scrollDirection: Axis.vertical,
+                         itemCount: snapshot.data!.length,
+                         itemBuilder: (context, index) {
+
+                          return Text(snapshot.data![index].userName.toString());
+                         });
+
+                     //  return Container(
+                     //    padding: EdgeInsets.only(left: 30),
+                     //  color: Colors.yellow,
+                     //  width: double.infinity,
+                     //   height: 50,
+                     //   child: Center(child: Text(snapshot.data![index].agentId.toString()))
+                     // );
+                    }
+
+                   }),
+
+                 // snapshot.data![index].agentId.toString(),
+
+              ]
+
+
+              //     child: ListView.builder(
+              // itemCount: snapshot.data?.length,
+              //     itemBuilder: (context, index) {
+              //       return Text(snapshot.data![index].agentId.toString());
+              //     }),
+              // Text(childlist[0].userName.toString(),style: TextStyle(color: Colors.black54,fontSize: 20),)
+                  // ListView.builder(
+                  //     itemCount: childlist.length,
+                  //     itemBuilder: (context, index) {
+                  //       return Text(childlist[index].userName.toString(),style: TextStyle(color: Colors.black54,fontSize: 20),);
+                  //     })
+
+                // child: Row(
+                //
+                //   children: [
+                //     Text(datalist[index].agentId.toString(),style: TextStyle(color: Colors.red,fontSize: 16),),
+                //     SizedBox(width: 20),
+                //     Text(datalist[index].userName.toString(),style: TextStyle(color: Colors.red,fontSize: 16)),
+                //     SizedBox(width: 20),
+                //     Text(datalist[index].agentId.toString(),style: TextStyle(color: Colors.red,fontSize: 16)),
+                //     SizedBox(width: 20),
+                //     Text(datalist[index].agentId.toString(),style: TextStyle(color: Colors.red,fontSize: 16)),
+                //   ],
+                // ),
+              );
+            }),
+        ),
       ),
-
     );
+
+
+
+
+
   }
 
- Widget buildTile(BasicTile e,{double leftPadding =16}) {
-    if(e.tiles.isEmpty){
-      return ListTile(
-        contentPadding: EdgeInsets.only(left: leftPadding+50),
-        title: Text(e.title!),
-
-      );
-    }else {
-      return ExpansionTile(title: Text(e.title!),
-        trailing: const SizedBox.shrink(),
-        leading: const Icon(Icons.add),
-        tilePadding: EdgeInsets.only(left: leftPadding),
-        children: e.tiles.map((e)=>buildTile(e,leftPadding: 16+leftPadding)).toList(),
-
-
-      );
-    }
-    }
-  Future apiparent() async {
+ // Widget buildTile(BasicTile e,{double leftPadding =16}) {
+ //    if(e.tiles.isEmpty){
+ //      return ListTile(
+ //        contentPadding: EdgeInsets.only(left: leftPadding+50),
+ //        title: Text(e.title!),
+ //
+ //      );
+ //    }else {
+ //      return ExpansionTile(title: Text(e.title!),
+ //        trailing: const SizedBox.shrink(),
+ //        leading: const Icon(Icons.add),
+ //        tilePadding: EdgeInsets.only(left: leftPadding),
+ //        children: e.tiles.map((e)=>buildTile(e,leftPadding: 16+leftPadding)).toList(),
+ //
+ //
+ //      );
+ //    }
+ //    }
+  Future <FirstModel?> apiparent() async {
     var _URL = "https://testapi.cptinternational.com/api/Agents/SearchAgentTree";
     Uri uri = Uri.parse(_URL);
-var token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjU4M2YxZTMxLTI3ZDEtNDI0NS1iNjNiLTE3NzA4Nzc1YzgzMiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkEiLCJleHAiOjE2NjIxMDE1MDMsImlzcyI6IlVzZXJDZW50ZXIiLCJhdWQiOiJhbGwifQ.QADFEE6N3tPAoSfy2Jr8Fgtj2HgzORxeHVViLyDT5wA";
+    var token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjNkMzA2ZjlmLTdmMjQtNDIzOC1iN2NmLWVmOGJkYjFhMzIwNyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkMiLCJleHAiOjE2NjIxODUyODcsImlzcyI6IlVzZXJDZW50ZXIiLCJhdWQiOiJhbGwifQ.JnhfMUMHVte7NhT60-it9yJ40dG5LySO9JXaYDztjZQ";
     var response = await http.post(uri,headers: <String,String>{
     'Content-Type': 'application/json',
     'Accept': 'application/json',
@@ -96,13 +195,17 @@ var token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2Fw
       "search": "Y"
     }),
     );
-    var data = jsonDecode(response.body);
+
     if(response.statusCode==200){
-      setState(() {
-        datalist=data
-      });
-      print("hello hemant");
-      var hemant=data['message'];
+
+
+
+
+      return FirstModel.fromJson(json.decode(response.body));
+      // return FirstModel(statusCode: aa['statusCode'],message: aa['message'],data: aa['data']);
+        // return fm.data;
+
+
 
     }
   }
